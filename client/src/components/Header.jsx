85fef571015c53473431596e6ceb64c8d2ce4d31
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-07 16:42:58 +0300

    added basic header layout,styles with links to different pages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <h1>App Name</h1>
+
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/photos'}>Photos</Link>
+        <Link to={'/users'}>Users</Link>
+        <Link to={'/Contacts'}>Contacts</Link>
+      </div>
+
+      <div>
+        <Link to={'/login'}>Login</Link>
+        <Link to={'/register'}>Register</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
