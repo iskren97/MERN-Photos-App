20fea493c70193171da310453128b31db01aacf5
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-07 16:30:21 +0300

    added routes in app component based on requirements

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Layout from './components/Layout';
+import Profile from './pages/Profile';
+import Photos from './pages/Photos';
+import Users from './pages/Users';
+import Contacts from './pages/Contacts';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/photos" element={<Photos />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
