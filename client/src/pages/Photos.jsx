0d2106f9ef7df5b84a05d6ae9246ab2acf28ee69
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-07 19:24:28 +0300

    photos page base logic

--- client/src/pages/Photos.jsx
+++ client/src/pages/Photos.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Photos = () => {
-  return <div>Photos</div>;
+  const [photos, setPhotos] = useState([]);
+
+  const fetchPhotos = async (e) => {
+    try {
+      const { data } = await axios.get('/photos');
+      console.log(data);
+      setPhotos(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Photos</h1>
+
+      <ul>
+        {photos.map((photo) => {
+          return <img src={photo.imgUrl} alt="" srcset="" />;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Photos;
