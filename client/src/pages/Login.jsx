e80c3a8aee6653aa876a1f019e4222b7c00980a6
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-07 17:42:44 +0300

    added login/register pages with corresponding requests, logic

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { Link, useNavigate } from 'react-router-dom';
+import Form from '../components/Form';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [_, setCookies] = useCookies(['access_token']);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/users/login', {
+        email,
+        password,
+      });
+
+      console.log(response);
+      setCookies('access_token', response.data.token);
+      window.localStorage.setItem('userID', response.data.user._id);
+      alert('Success');
+      navigate('/profile');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Form
+        label={'Login'}
+        email={email}
+        setEmail={handleEmailChange}
+        password={password}
+        setPassword={handlePasswordChange}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Form from '../components/Form';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleNameChange = (e) => setName(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('/users/register', {
+        name,
+        email,
+        password,
+      });
+
+      alert('Registration completed!');
+      navigate('/login');
+    } catch (error) {
+      alert('Error!');
+    }
+  };
+
+  return (
+    <>
+      <Form
+        label={'Register'}
+        email={email}
+        setEmail={handleEmailChange}
+        name={name}
+        setName={handleNameChange}
+        password={password}
+        setPassword={handlePasswordChange}
+        handleSubmit={handleSubmit}
+      />
+    </>
+  );
 };
 
 export default Register;
