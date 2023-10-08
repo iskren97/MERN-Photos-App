58e05879748db5c8b85ac8e720638174342e7a94
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-10-08 11:45:21 +0300

    fetched and presented user's photos in profile page, added delete func

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,10 +6,33 @@ import useUser from '../hooks/useUser';
 const Profile = () => {
   const [photos, setPhotos] = useState([]);
   const user = useUser();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios.get(`/photos/${user._id}`).then(({ data }) => setPhotos(data));
+    setIsLoading(true);
+    axios
+      .get(`/photos/${user._id}`)
+      .then(({ data }) => setPhotos(data))
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setIsLoading(false));
   }, []);
+
+  const handlePhotoDelete = (photoID) => {
+    setIsLoading(true);
+    axios
+      .delete(`/photos/${photoID}`)
+      .then(({ data }) => {
+        setPhotos((prevPhotos) =>
+          prevPhotos.filter((photo) => photo._id !== data._id)
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
 
   return (
     <div>
@@ -18,30 +41,31 @@ const Profile = () => {
       <h3>Upload a photo</h3>
       <ImageUpload />
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      <h3 style={{ marginTop: '100px' }}>Uploaded Photos:</h3>
 
-      <h3>Uploaded Photos:</h3>
-      {photos.length > 0 ? (
-        photos.map((photo) => {
-          return (
-            <img
-              key={photo._id}
-              style={{
-                width: '200px',
-                height: '200px',
-                objectFit: 'contain',
-              }}
-              src={photo.imgUrl}
-              alt="img"
-            />
-          );
-        })
-      ) : (
+      {isLoading ? (
         <p>Loading...</p>
+      ) : (
+        <div className="profile-photo-container">
+          {photos.length > 0 ? (
+            photos.map((photo) => {
+              return (
+                <div key={photo._id} className="photo-inner-container">
+                  <img className="photo" src={photo.imgUrl} alt="img" />
+
+                  <button
+                    onClick={() => handlePhotoDelete(photo._id)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })
+          ) : (
+            <p>You have no photos yet...</p>
+          )}
+        </div>
       )}
     </div>
   );
